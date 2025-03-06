0cdfa4f600242646920af7d017bcc0d4943933d3
ProjectSaveGH <alexander.schwarz148@gmail.com>
2025-03-06 04:09:49 +0100

    Update

--- extension.js
+++ extension.js
@@ -1,15 +1,10 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "tc-vscode" is now active!');
@@ -27,8 +22,9 @@ function activate(context) {
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 function deactivate() {}
+
+
 
 module.exports = {
 	activate,
